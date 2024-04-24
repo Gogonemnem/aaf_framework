c57cbfe3a1400213441373c3ecccd5db49155b67
Gonem <glau.github@pm.me>
2024-04-24 18:01:00 +0200

    try multi gpu

--- main.py
+++ main.py
@@ -1,10 +1,55 @@
+import os
 from .train.train import Trainer
-from .config import cfg
+from .config import cfg as config
 import sys
 import torch
+from torch import cuda
+from detectron2.engine import (
+    default_argument_parser,
+    default_setup,
+    default_writers,
+    launch,
+)
 
-cfg.merge_from_file('aaf_framework/config_files/fcos_R_50_FPN_DOTA.yaml')
+def set_cuda_visible_devices():
+    cuda_visible_devices = ','.join(str(i) for i in range(cuda.device_count()))
+    os.environ["CUDA_VISIBLE_DEVICES"] = cuda_visible_devices
+
+def setup(args):
+    """
+    Create configs and perform basic setups.
+    """
+    cfg = config.clone()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    # default_setup(
+    #     cfg, args
+    # )
+    return cfg
+    
+
+def main(args):
+    cfg = setup(args)
+    trainer = Trainer(cfg)
+    
+    if not args.eval_only:
+        trainer.train()
 
 
-trainer = Trainer(cfg)
-trainer.train()
+def invoke_main() -> None:
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
+
+
+if __name__ == "__main__":
+    invoke_main()  # pragma: no cover
+    
---
+++ train_fcos_PVT_V2_B2_LI_FPN_DIOR.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+cd  ~
+
+# Parse command-line arguments
+while getopts ":c:" opt; do
+ case ${opt} in
+    c )
+      CONFIG_FILE=$OPTARG
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit 1
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      exit 1
+      ;;
+ esac
+done
+
+CONFIG_FILE="aaf_framework/config_files/fcos_PVT_V2_B2_LI_FPN_DIOR.yaml"
+shift $((OPTIND -1))
+
+# Check if the configuration file exists
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: Configuration file $CONFIG_FILE does not exist."
+    exit 1
+fi
+
+# Log directory path, constructed based on the configuration file name
+LOG_DIR="logs/$(basename "$CONFIG_FILE" .yaml)"
+
+# Check if the log directory exists, create it if not
+if [ ! -d "$LOG_DIR" ]; then
+    mkdir -p "$LOG_DIR"
+fi
+
+# Determine the number of GPUs available
+NUM_GPUS=$(nvidia-smi -L | wc -l)
+
+# Set CUDA_VISIBLE_DEVICES to use all available GPUs
+CUDA_VISIBLE_DEVICES=$(seq -s, 0 $(($NUM_GPUS-1)))
+
+# Execute the command with the detected number of GPUs and configuration file
+.conda/envs/venv-nvidia/bin/python -m aaf_framework.main --num-gpus 1 --dist-url auto \
+        --config-file "$CONFIG_FILE" SOLVER.IMS_PER_BATCH 4 
