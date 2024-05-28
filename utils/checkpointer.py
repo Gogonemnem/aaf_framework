dbe877b4ea8c6bc10666cedaaacf0d1c5a426a5c
Gonem <glau.github@pm.me>
2024-05-28 16:17:29 +0200

    Refactor DetectronCheckpointer to allow loading forcefully from a specific file

--- utils/checkpointer.py
+++ utils/checkpointer.py
@@ -3,18 +3,21 @@ import os
 from fcos.core.utils.checkpoint import DetectronCheckpointer as fcosDetectronCheckpointer
 
 class DetectronCheckpointer(fcosDetectronCheckpointer):
-    None
-    # def get_checkpoint_file(self):
-    #     save_file = os.path.join(self.save_dir, "last_checkpoint")
-    #     try:
-    #         with open(save_file, "r") as f:
-    #             last_saved = f.read()
-    #             last_saved = last_saved.strip()
-    #     except IOError:
-    #         # if file doesn't exist, maybe because it has just been
-    #         # deleted by a separate process
-    #         last_saved = ""
-    #     return os.path.join(self.save_dir, last_saved)
+    def __init__(self, cfg, model, optimizer=None, scheduler=None, save_dir="", save_to_disk=None, logger=None):
+        super().__init__(cfg, model, optimizer, scheduler, save_dir, save_to_disk, logger)
+        self.file = None
+
+    def load(self, f=None, force_file=False):
+        
+        if force_file and f is not None:
+            self.file = f
+
+        return super().load(f)
+
+    def get_checkpoint_file(self):
+        if self.file is not None:
+            return self.file
+        return super().get_checkpoint_file()
 
 
 
